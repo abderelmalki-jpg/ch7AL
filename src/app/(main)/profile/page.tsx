6c6c081e6dfac4a10eb06d1114c032ab96e82887
Abderrahman El malki <elmalkiabder@gmail.com>
2025-10-29 22:20:12 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
