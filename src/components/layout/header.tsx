d868225da6b30f983b3985179331b60f075fa588
Abderrahman El malki <elmalkiabder@gmail.com>
2025-10-29 22:42:34 +0000

    https://res.cloudinary.com/dhjwimevi/image/upload/v1761777698/ChatGPT_Im

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         <div className="w-10"></div> {/* Spacer to balance the layout */}
         <Link href="/dashboard" className="flex justify-center">
-          <Logo className="h-12" />
+          <Logo className="h-16 w-16" />
         </Link>
         <Link href="/profile">
           <Avatar className="h-9 w-9 border-2 border-primary">
