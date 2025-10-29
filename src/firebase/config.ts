ff2228ba32180d259bca31641b119b4a316c5478
Abderrahman El malki <elmalkiabder@gmail.com>
2025-10-29 19:28:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3669303802-34286",
+  "appId": "1:231110465093:web:30ebee0428d1d47227f52c",
+  "apiKey": "AIzaSyDMM0MvKxuvHaPGxRCteaAvTOskP6aO384",
+  "authDomain": "studio-3669303802-34286.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "231110465093"
+};
