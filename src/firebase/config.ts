95d22143e00fce0653522ea613c296d289549e05
Abderrahman El malki <elmalkiabder@gmail.com>
2025-10-29 23:09:24 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "hanouti-6ce26",
-  "appId": "1:252246765953:web:726dc032c6eeba126bb880",
-  "storageBucket": "hanouti-6ce26.appspot.com",
+  "appId": "1:252246765953:web:e6d221845cc3c2e86bb880",
   "apiKey": "AIzaSyAJuQu1zUjvst6GevnVUAJe17ew7PhxODs",
   "authDomain": "hanouti-6ce26.firebaseapp.com",
+  "measurementId": "",
   "messagingSenderId": "252246765953"
 };
