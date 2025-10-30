de8b4b7b0e2dfa913eb77a901e2ce3f0f8ea98c4
Abderrahman El malki <elmalkiabder@gmail.com>
2025-10-30 01:22:01 +0000

    D de l'application 1:252246765953:web:726dc032c6eeba126bb880Site Firebas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "hanouti-6ce26",
-  "appId": "1:252246765953:web:e6d221845cc3c2e86bb880",
-  "apiKey": "AIzaSyAJuQu1zUjvst6GevnVUAJe17ew7PhxODs",
-  "authDomain": "hanouti-6ce26.firebaseapp.com",
-  "storageBucket": "hanouti-6ce26.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "252246765953"
+  projectId: "hanouti-6ce26",
+  appId: "1:252246765953:web:726dc032c6eeba126bb880",
+  apiKey: "AIzaSyAJuQu1zUjvst6GevnVUAJe17ew7PhxODs",
+  authDomain: "hanouti-6ce26.firebaseapp.com",
+  storageBucket: "hanouti-6ce26.appspot.com",
+  messagingSenderId: "252246765953",
 };
