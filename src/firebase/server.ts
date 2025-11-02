20cec6c08a09330d522203c1df5a9be4c24182d1
Abderrahman El malki <elmalkiabder@gmail.com>
2025-11-02 02:09:04 +0000

    Try fixing this error: `Console Error: Error: Maximum call stack size ex

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -21,7 +21,7 @@ function initializeAdminApp() {
     const serviceAccountKey = {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
     };
 
     if (serviceAccountKey.projectId && serviceAccountKey.clientEmail && serviceAccountKey.privateKey) {
